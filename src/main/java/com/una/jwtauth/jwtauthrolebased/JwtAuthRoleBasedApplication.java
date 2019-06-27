68e2afaddf62e70a5b6b61603d3edc6a198782c4
teten777 <teten.nugraha18@gmail.com>
2019-06-27 13:43:03 +0700

    first

---
+++ src/main/java/com/una/jwtauth/jwtauthrolebased/JwtAuthRoleBasedApplication.java
@@ -0,0 +1,13 @@
+package com.una.jwtauth.jwtauthrolebased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthRoleBasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthRoleBasedApplication.class, args);
+    }
+
+}
