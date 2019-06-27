72a56797f1dc57940afb5306d2688569a4abdcf1
teten777 <teten.nugraha18@gmail.com>
2019-06-27 13:59:14 +0700

    CORSFilter

---
+++ src/main/java/com/una/jwtauth/jwtauthrolebased/config/CORSFilter.java
@@ -0,0 +1,30 @@
+package com.una.jwtauth.jwtauthrolebased.config;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+        System.out.println("Filtering on .................");
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
